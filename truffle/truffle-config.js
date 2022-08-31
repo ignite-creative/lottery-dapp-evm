946495cd7ad8fab6635801537bbfdb8e45f762b7
prodevcn <cn.corestar@gmail.com>
2022-08-31 22:41:44 +0800

    initial commit

---
+++ truffle/truffle-config.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+
+const infuraApiKey = process.env.INFURA_API_KEY;
+const moonbaseUrl = process.env.MOONBASE_URL;
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
+const adminPrivKey = process.env.ADMIN_PRIV_KEY;
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  contracts_build_directory: "../client/src/contracts",
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    moonbase: {
+      provider: () => new HDWalletProvider(admin, moonbaseUrl),
+      network_id: 1287,       // Goerli's network id
+      chain_id: 1287,         // Goerli's chain id
+      gas: 5500000,        // Gas limit used for deploys.
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets)
+    },
+    //
+    // Useful for private networks
+    // private: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraProjectId}`),
+    //   network_id: 2111,   // This network is yours, in the cloud.
+    //   production: true    // Treats this network as if it was a public net. (default: false)
+    // }
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.14",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled:
+  // false to enabled: true. The default storage location can also be
+  // overridden by specifying the adapter settings, as shown in the commented code below.
+  //
+  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+  // make a backup of your artifacts to a safe location before enabling this feature.
+  //
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
+  // $ truffle migrate --reset --compile-all
+  //
+  // db: {
+  //   enabled: false,
+  //   host: "127.0.0.1",
+  //   adapter: {
+  //     name: "sqlite",
+  //     settings: {
+  //       directory: ".db"
+  //     }
+  //   }
+  // }
+};
